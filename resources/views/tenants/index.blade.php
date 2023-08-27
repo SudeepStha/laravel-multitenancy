a0915bc97993570045b9654cb49c205499eb30f4
tshrestha <tej@mavorion.com>
2023-08-27 18:01:44 +0545

    crud of tenant and to login route of different tenant

---
+++ resources/views/tenants/index.blade.php
@@ -0,0 +1,41 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tenants') }}
+            <x-btn-link class="ml-4 float-right" href="{{ route('tenants.create') }}">Add Tenant</x-btn-link>
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Domain Name</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tenants as $tenant)
+                                <tr>
+                                    <td>{{ $tenant->name }}</td>
+                                    <td>{{ $tenant->email }}</td>
+                                    <td>
+                                        @foreach ($tenant->domains as $domain)
+                                            {{ $domain->domain }} {{ $loop->last ? '' : ',' }}
+                                        @endforeach
+                                    </td>
+                                    <td>Edit</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
